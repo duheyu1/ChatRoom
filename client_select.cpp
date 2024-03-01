76362221a63ace3b28119a00c0fb78013ab3d557
leiyue <leiyue159@gmail.com>
2024-03-01 04:17:17 -0500

    version 0.2

--- client_select.cpp
+++ client_select.cpp
@@ -3,36 +3,39 @@
 #define MAXLINE 500
 
 int max(int i, int j) {
-    if (i > j)
-        return i;
-    else
-        return j;
+    if (i > j) return i;
+    return j;
 }
 
-int main() {
+int main(int argc, char **argv) {
+
+	if (argc != 2) printf("two few argument\n");
 	
 	// preparations
 	int fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    fd_set rset;
-    char sendline[MAXLINE], recvline[MAXLINE];
 	sockaddr_in servaddr;
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(51453);
-	inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr);
+	inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
 
 	connect(fd, (struct sockaddr*)&servaddr, sizeof(servaddr));
-	printf("connect success\n");
 
+	// main loop, check for input and socket
+    fd_set rset;
+    char sendline[MAXLINE], recvline[MAXLINE];
     FD_ZERO(&rset);
 	for ( ; ; ) {
         FD_SET(fd, &rset);
         FD_SET(fileno(stdin), &rset);
+		fflush(stdout);
         select(max(fileno(stdin), fd) + 1, &rset, NULL, NULL, NULL);
 
+		// check for socket
         if ( FD_ISSET(fd, &rset) ) {
-		    int n = read(fd, recvline, MAXLINE);
+			sprintf(recvline, "%s", "as: ");
+		    int n = read(fd, &recvline[4], MAXLINE - 4);
 		    if (n == 0) {
 			    printf("close normally\n");
 			    exit(0);
@@ -41,15 +44,15 @@ int main() {
 			    perror("failure\n");
 			    exit(-1);
 		    }
-		    recvline[n] = '\0';
-		    printf("%s", recvline);
+		    recvline[n + 4] = '\0';
+		    fputs(recvline, stdout);
         }
 
+		// check for input
         if ( FD_ISSET(fileno(stdin), &rset) ) {
-            fgets(sendline, 500, stdin);
+            fgets(sendline, MAXLINE, stdin);
 		    int byte_sent = write(fd, sendline, strlen(sendline));
 		    if (byte_sent == -1) exit(-1);
-		    sendline[0] = '\0';
         }
     }
 
--- server_select.cpp
+++ server_select.cpp
@@ -3,21 +3,15 @@
 #define MAXLINE 500
 
 int max(int i, int j) {
-    if (i > j)
-        return i;
-    else
-        return j;
+    if (i > j) return i;
+    return j;
 }
 
 int main() {
     	
-	printf("haha\n");
 	// preparations
 	int listenfd = socket(AF_INET, SOCK_STREAM, 0);
-	printf("socket success\n");
 
-    fd_set rset;
-    char sendline[MAXLINE], recvline[MAXLINE];
 	sockaddr_in servaddr, cliaddr;
 	socklen_t clilen;
 	bzero(&servaddr, sizeof(servaddr));
@@ -27,24 +21,25 @@ int main() {
 
 	int opt = 1;
 	setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-
 	bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
-	printf("bind success\n");
-
 	listen(listenfd, 5);
-	printf("listen success\n");
 
 	int connfd = accept(listenfd, (struct sockaddr*)&cliaddr, &clilen);
-	printf("connect success %d\n", connfd);
 
+	// main loop, check for input and socket
+	fd_set rset;
+    char sendline[MAXLINE], recvline[MAXLINE];
     FD_ZERO(&rset);
 	for ( ; ; ) {
         FD_SET(connfd, &rset);
         FD_SET(fileno(stdin), &rset);
+		fflush(stdout);
         select(max(fileno(stdin), connfd) + 1, &rset, NULL, NULL, NULL);
 
+		// check for socket
         if ( FD_ISSET(connfd, &rset) ) {
-		    int n = read(connfd, recvline, MAXLINE);
+			sprintf(recvline, "%s", "as: ");
+		    int n = read(connfd, &recvline[4], MAXLINE - 4);
 		    if (n == 0) {
 			    printf("close normally\n");
 			    exit(0);
@@ -53,15 +48,15 @@ int main() {
 			    perror("failure\n");
 			    exit(-1);
 		    }
-		    recvline[n] = '\0';
-		    printf("%s", recvline);
+		    recvline[n + 4] = '\0';
+		    fputs(recvline, stdout);
         }
 
+		// check for input
         if ( FD_ISSET(fileno(stdin), &rset) ) {
-            fgets(sendline, 500, stdin);
+            fgets(sendline, MAXLINE, stdin);
 		    int byte_sent = write(connfd, sendline, strlen(sendline));
 		    if (byte_sent == -1) exit(-1);
-		    sendline[0] = '\0';
         }
     }
 
